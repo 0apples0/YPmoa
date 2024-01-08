34a92d32ef5d27f483711bee60ee19c87ec26edf
geonhyuk <wwkwkddl7573@gmail.com>
2024-01-08 17:22:45 +0900

    naver xml추가

--- src/main/java/com/moa/youthpolicy/login/kakao/KakaoTokenResponse.java
+++
@@ -1,5 +0,0 @@
-package com.moa.youthpolicy.login.kakao;
-
-public class KakaoTokenResponse {
-
-}
--- src/main/java/com/moa/youthpolicy/user/service/UserService.java
+++ src/main/java/com/moa/youthpolicy/user/service/UserService.java
@@ -2,8 +2,8 @@ package com.moa.youthpolicy.user.service;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class UserService {
-	private static final String KAKAOADMINID = "7e3903a1e0391c34512d044489e0be1f";
 	
 }
