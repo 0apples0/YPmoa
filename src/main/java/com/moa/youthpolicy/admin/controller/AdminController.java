b50fd091810d4ceacf1b74aaabdeeb647e4d3adc
bobin531 <parkbobin38@gmail.com>
2024-01-08 12:23:07 +0900

    bb

--- src/main/java/com/moa/youthpolicy/admin/controller/AdminController.java
+++ src/main/java/com/moa/youthpolicy/admin/controller/AdminController.java
@@ -1,5 +1,5 @@
 package com.moa.youthpolicy.admin.controller;
 
 public class AdminController {
-	// 어드민 컨트롤러
+	// qq
 }
--- src/main/java/com/moa/youthpolicy/common/BoardDTO.java
+++ src/main/java/com/moa/youthpolicy/common/BoardDTO.java
@@ -1,6 +1,5 @@
 package com.moa.youthpolicy.common;
 
-// 보빈 깃 테스트
 public class BoardDTO {
 
 }
