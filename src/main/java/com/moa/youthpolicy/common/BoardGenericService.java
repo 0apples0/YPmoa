5c8fbc7d49a70852dab787f422b92bf9ccbed8c7
geonhyuk <wwkwkddl7573@gmail.com>
2024-01-08 14:35:51 +0900

    interface

--- src/main/java/com/moa/youthpolicy/common/BoardGenericService.java
+++ src/main/java/com/moa/youthpolicy/common/BoardGenericService.java
@@ -1,7 +1,7 @@
 package com.moa.youthpolicy.common;
 
 public interface BoardGenericService extends BoardInterface {
-	public <T> void writeBoard(T boardVO); // 글 쓰기 
+	public <T> void writeBoard(T boardVO); // 글 쓰기
 	public <T> void toggleLike(T boardVO); //글 좋아요 or 취소
 	public void getBack(); // 뒤로가기
 }
