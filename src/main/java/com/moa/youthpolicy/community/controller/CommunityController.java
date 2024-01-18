6c7e937394bfbf17e4950702aeb0cc9535651195
Yumin_Lee <aefaefsafdsfdsax2123123@gmail.com>
2024-01-18 15:10:11 +0900

    이미지 업로드 기능 구현을 위한 일부 코드 변경

--- src/main/java/com/moa/youthpolicy/community/controller/CommunityController.java
+++ src/main/java/com/moa/youthpolicy/community/controller/CommunityController.java
@@ -1,6 +1,14 @@
 package com.moa.youthpolicy.community.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.FileUtils;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.google.gson.JsonObject;
 import com.moa.youthpolicy.common.Criteria;
 import com.moa.youthpolicy.common.PageDTO;
 import com.moa.youthpolicy.community.domain.CommunityCommentVO;
@@ -58,7 +68,84 @@ public class CommunityController {
 	//글 작성 페이지로 이동
 	@GetMapping("/write")
 	public void getWrite() {}
+
 	
+	//private String realPath = "C:\summernote_image";
+	/*
+	@RequestMapping(value="/uploadSummernoteImageFile", produces = "application/json; charset=utf8")
+	@ResponseBody
+	public String uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile, HttpServletRequest request )  {
+		JsonObject jsonObject = new JsonObject();
+		log.info("파일업로드 ajax 호출은 되었어요");
+        
+		String contextRoot = realPath + "/upload_image/image/fileupload/29/";
+		String fileRoot = contextRoot;
+		//String fileRoot = "C:\\summernote_image\\"; // 외부경로로 저장을 희망할때.
+		
+		
+		// 내부경로로 저장
+		//String contextRoot = new HttpServletRequestWrapper(request).getRealPath("/");
+		//String fileRoot = contextRoot+"resources/fileupload_community/";
+		//log.info("컨텍스트 루트에요"+contextRoot);
+		//String root = request.getSession().getServletContext().getRealPath("resources");
+		//log.info("파일 루트에요"+root);
+		log.info("파일 루트에요"+fileRoot);
+		String originalFileName = multipartFile.getOriginalFilename();	//오리지널 파일명
+		String extension = originalFileName.substring(originalFileName.lastIndexOf("."));	//파일 확장자
+		String savedFileName = UUID.randomUUID() + extension;	//저장될 파일 명
+		log.info("파일업로드 파일이름이에요"+savedFileName);
+		File targetFile = new File(fileRoot + savedFileName);	
+		log.info("파일업로드 타겟파일이에요"+targetFile.toString());
+		try {
+			InputStream fileStream = multipartFile.getInputStream();
+			FileUtils.copyInputStreamToFile(fileStream, targetFile);	//파일 저장
+			jsonObject.addProperty("url", "/upload_image/image/fileupload/29/"+savedFileName); // contextroot + resources + 저장할 내부 폴더명
+			jsonObject.addProperty("responseCode", "success");
+				
+		} catch (IOException e) {
+			FileUtils.deleteQuietly(targetFile);	//저장된 파일 삭제
+			jsonObject.addProperty("responseCode", "error");
+			e.printStackTrace();
+		}
+		String a = jsonObject.toString();
+		return a;
+	}	
+
+
+	@Configuration
+	public class servletConfig implements WebMvcConfigurer {
+
+	    @Bean
+	    public ViewResolver viewResolver(){
+	        // VIEW 지정
+	        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	        // 접두사(ex./WEB-INF/views/main.jsp)
+	        resolver.setPrefix("/WEB-INF/View/");
+	        // 접미사
+	    
+	        resolver.setSuffix(".jsp");
+	        
+	        return resolver;
+	    }
+
+	    // 주소창 입력값
+	    private String connectPath = "/upload_image/**";
+
+	    // 업로드 파일  위치
+	    private String resourcePath = "file:///tomcat/webapps/upload_image/";
+
+	    @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        
+	        // 업로드 이미지 정적 리소스 설정
+	        registry.addResourceHandler(connectPath).addResourceLocations(resourcePath);
+
+	        // 기본 정적 파일 설정
+	        registry.addResourceHandler("resources/**").addResourceLocations("classpath:/static/");
+	    }
+
+	    
+	}	*/
 	// Ajax가 호출하는 메서드, 반환타입은 json으로 설정하라는 주석
 	@ResponseBody
 	@RequestMapping(value="/getList", method=RequestMethod.POST)
